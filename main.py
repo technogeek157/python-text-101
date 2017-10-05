350780e6495b426d1afb11c7b8a2ae4eba194ee6
technogeek157 <32140456+technogeek157@users.noreply.github.com>
2017-10-05 13:36:56 -0500

    completed basic state engine
    state engine uses functions

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-currentState = 'dungeon_01'
-states = ["dungeon_01", "door_01", "torch_01", "corridor_01", "dungeon_02", "chest_02", "door_02", "torch_02", "corridor_02", 
-					"grue", "dungeon_03", "chest_03", "torch_03", "corridor_03"]
-statechange = true
+global currentstate
+currentstate = "dungeon_01"
+global define
 
-while true: #main loop, starts functions
-	if currentState == "dungeon_01":
-		state_dungeon_01()
-
-def state_dungeon_01(): #this is one of the states
+def state_dungeon_01(currentstate): #this is one of the states
 	print("You are in a dank dungeon. There are stone walls all around. There is a wooden chest and a lit torch on the wall.\n\n" +
 				"Press C to open the Chest, D to try the Door, and T to look at the Torch")
 	playerinput = raw_input()
-	if playerinput == "C":
+	playerinput = playerinput.lower()
+	if playerinput == "c":
 		currentstate = "chest_01"
-	elif playerinput == "D":
+	elif playerinput == "d":
 		currentstate = "door_01"
-	elif playerinput == "T":
+	elif playerinput == "c":
 		currentstate = "torch_01"
+	return currentstate
+
+def state_chest_01():
+	print("The chest is almost empty, but there is a unlit torch inside.\n\n" +
+	      "Press R to Return")
+	playerinput = raw_input()
+	if playerinput == "r":
+		currentstate = "dungeon_01"
+	return currentstate
+
+while True: #main loop, starts functions
+	if currentstate == "dungeon_01":
+		currentstate = state_dungeon_01(currentstate)
+	elif currentstate == "chest_01":
+		currentstate = state_chest_01()
