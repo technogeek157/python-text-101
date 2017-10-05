4395ccf1a744e764372529813918df802c576d4e
technogeek157 <32140456+technogeek157@users.noreply.github.com>
2017-10-05 13:53:08 -0500

    expanded states

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def state_dungeon_01(currentstate): #this is one of the states
 		currentstate = "chest_01"
 	elif playerinput == "d":
 		currentstate = "door_01"
-	elif playerinput == "c":
+	elif playerinput == "t":
 		currentstate = "torch_01"
 	return currentstate
 
@@ -23,8 +23,46 @@ def state_chest_01():
 		currentstate = "dungeon_01"
 	return currentstate
 
+def state_door_01():
+	print("This is a old dungeon door. It appears to be unlocked. \n\n" +
+	      "Press C to continue, R to Return")
+	playerinput = raw_input()
+	if playerinput == "r":
+		currentstate = "dungeon_01"
+	elif playerinput == "c":
+		currentstate = "corridor_01"
+	return currentstate
+
+def state_torch_01():
+	print("This is a torch, it burnes with a orange light, illuminating the room.\n\n" +
+	      "Press R to Return")
+	playerinput = raw_input()
+	if playerinput == "r":
+		currentstate = "dungeon_01"
+	return currentstate
+
+def state_corridor_01():
+	print("A long corridor stretches before you, darkness concealing the end. You are likely to be eaten by a grue.\n\n" +
+	      "Press C to continue, R to Return")
+	playerinput = raw_input()
+	if playerinput == "r":
+		currentstate = "dungeon_01"
+	elif playerinput == "c":
+		currentstate = "grue"
+	return currentstate
+
+
 while True: #main loop, starts functions
 	if currentstate == "dungeon_01":
 		currentstate = state_dungeon_01(currentstate)
 	elif currentstate == "chest_01":
 		currentstate = state_chest_01()
+	elif currentstate == "door_01":
+		currentstate = state_door_01()
+	elif currentstate == "torch_01":
+		currentstate = state_torch_01()
+	elif currentstate == "corridor_01":
+		currentstate = state_corridor_01()
+
+
+		
