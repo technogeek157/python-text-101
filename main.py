6b79eba867698339e86fb034cd95f10f1d56b836
Luka Thelen <32140456+technogeek157@users.noreply.github.com>
2017-10-10 13:48:03 -0500

    finished new state system, wrote out to current end

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ global keyDict
 global newState
 newState = ""
 returnkey = "Press R to Return"
+chestPoint = False
 
 def one_room(situation, gotos, keyOne, gotoOne):
     print("\n\n" + situation + "\n\n" + gotos)
@@ -37,6 +38,7 @@ def three_room(situation, gotos, keyOne, keyTwo, keyThree, gotoOne, gotoTwo, got
       return gotoThree
     else:
       return("kpe")
+      
 
 while True:
     if currentstate == "dungeon_01":
@@ -67,7 +69,47 @@ while True:
       
     if currentstate == "dungeon_02":
       newState = three_room("You are in a dark dungeon, there is a torch on the wall, " +
-      "a chest in the corner, and a closed wood and iron door leading out. You have a unlit torch in your hand", "Press C to look at the Chest, T to look at the Torch, and D to look at the Door", 'c', 't', 'd', "chest_02", "torch_02", "door_02")
+      "a chest in the corner, and a closed wood and iron door leading out. You have a unlit torch in your hand",
+      "Press C to look at the Chest, T to look at the Torch, and D to look at the Door", 
+      'c', 't', 'd', "chest_02", "torch_02", "door_02")
+      
+    if currentstate == "chest_02":
+      newState = one_room("Nope, nothing here.", returnkey, 'r', "dungeon_02")
+         
+    if currentstate == "door_02":
+      newState = two_room("An old dungeon door. It appears to be unlocked.",
+      returnkey + ", C to Continue", 'r', 'c', "dungeon_02", "corridor_02")
+      
+    if currentstate == "torch_02":
+      newState = two_room("Well, are you just going to stand there, or light the torch?", 
+      returnkey + ", L to Light your torch.", 'r', 'l', 'dungeon_02', 'dungeon_03')
+      
+    if currentstate == "corridor_02":
+      newState = two_room("A dark corridor stretches before you. If it has a end, " +
+      "darkness conceals it. You are likely to be eaten by a Grue.", 
+      returnkey + ", C to Continue", 'r', 'c', "dungeon_02", "Grue")
+      
+    if currentstate == "dungeon_03":
+      newState = three_room("The dungeon burns slightly brighter with the light of your torch.",
+      "D to try the Door, C to look in the Chest, T to inspect the Torch", 
+      'd', 'c', 't', 'door_03', "chest_03", "torch_03")
+      
+    if currentstate == "chest_03":
+      newState = one_room("Well, aren't you persistent! Here's a bonus point for you!", returnkey,
+      'r', "dungeon_03")
+      chestPoint = True
+      
+    if currentstate == "torch_03":
+      newState = one_room("Yep, still burning, but it might not burn forever, and the the Grues will come out",
+      returnkey, 'r', "dungeon_03")
+      
+    if currentstate == "door_03":
+      newState = two_room("An old dungeon door. It appears to be unlocked.",
+      returnkey + ", C to Continue", 'r', 'c', "dungeon_03", "corridor_03")
+      
+    if currentstate == "corridor_03":
+      newState = two_room("At the end of the corridor, there is a door, set in stone.", returnkey + 
+      ", C to Continue", 'r', 'c', "dungeon_03", " ")
       
     if newState != "kpe":
       currentstate = newState
